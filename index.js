9b93edffb7b3930324cd1abd853a179085a9f34c
Lenna <lenna@lennamau.com>
2019-02-19 14:05:59 -0700

    Initial commit
    dependencies and database created
    knex configuration

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex')
+
+const knexConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection : {
+        filename: './data/zoos.db3'
+    } 
+}
+
+const db = knex(knexConfig)
 const server = express();
 
 server.use(express.json());
@@ -8,7 +19,11 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
+server.post('/api/zoos', (req, res) => {
+  
+});
+
+const port = 4000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
